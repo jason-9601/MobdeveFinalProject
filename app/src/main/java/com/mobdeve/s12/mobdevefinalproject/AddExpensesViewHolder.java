dfafa60daf1e34a1a9e25fa4cfd229e5f446c82a
jason-9601 <jason_alon-alon@dlsu.edu.ph>
2021-08-20 17:05:12 +0800

    Created AddExpensesAdapter and AddExpensesViewHolder for recyclerview in AddExpensesInput page

---
+++ app/src/main/java/com/mobdeve/s12/mobdevefinalproject/AddExpensesViewHolder.java
@@ -0,0 +1,36 @@
+package com.mobdeve.s12.mobdevefinalproject;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class AddExpensesViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView tvExpenseTitle;
+    private TextView tvExpenseDate;
+    private TextView tvExpenseValue;
+
+    public AddExpensesViewHolder(@NonNull @NotNull View itemView) {
+        super(itemView);
+        tvExpenseTitle = itemView.findViewById(R.id.tv_expense_title);
+        tvExpenseDate = itemView.findViewById(R.id.tv_expense_date);
+        tvExpenseValue = itemView.findViewById(R.id.tv_expense_value);
+    }
+
+    public void setTvExpenseTitle(String text) {
+        this.tvExpenseTitle.setText(text);
+    }
+
+    public void setTvExpenseDate(String text) {
+        this.tvExpenseDate.setText(text);
+    }
+
+    public void setTvExpenseValue(String text) {
+        this.tvExpenseValue.setText(text);
+    }
+
+}
